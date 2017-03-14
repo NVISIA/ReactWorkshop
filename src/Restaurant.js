b6555d6244d59892a5a4ca6c83edb1531386001c
Joshua Armstrong <jarmstrong@nvisia.com>
2017-03-14 08:02:38 -0500

    Add waffle-grid for 12-column grid layout.

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react'
 
 class Restaurant extends Component {
+  constructor (props) {
+    super(props)
+    this.id = props.id
+    this.name = props.name || ''
+    this.tagline = props.tagline || ''
+    this.price = props.price || ''
+    this.rating = props.rating || ''
+    this.address = props.address || ''
+    this.description = props.description || ''
+    this.availableTimes = props.availableTimes || []
+  }
+
   render () {
-    return (<div className='Restaurant'><h1>Restaurant</h1></div>)
+    return (<div className='Restaurant'><h1>{this.name}</h1></div>)
   }
 }
 
 class RestaurantList extends Component {
   render () {
-    return (<div className='RestaurantList'><h1>Restaurant List</h1></div>)
+    return (
+      <div className='RestaurantList'>
+        <h1>Restaurant List</h1>
+        {this.props.items.map(item => (
+          <Restaurant
+            key={item.id}
+            name={item.name}
+            tagline={item.tagline}
+            price={item.price}
+            rating={item.rating}
+            address={item.address}
+            description={item.description}
+            availableTimes={item.availableTimes}
+          />
+        ))}
+      </div>
+    )
   }
 }
 
