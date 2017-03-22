682e660bc5b8545db48ef26d52a461a2035b69a5
Joshua Armstrong <jarmstrong@nvisia.com>
2017-03-22 10:26:55 -0500

    Add unit test for componentDidMount
    Ensure it calls fetchRestaurants.

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-/* global it, describe, expect, beforeEach, afterEach */
+/* global it, describe, expect, beforeEach, afterEach, spyOn */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import TestUtils from 'react-addons-test-utils'
@@ -37,6 +37,17 @@ describe('DataContainer', () => {
     expect(component.state.restaurants).toEqual([])
     expect(component.state.restaurant).toBeDefined()
     expect(component.state.restaurant).toEqual({})
+  })
+
+  it('should call fetchRestaurants after mounting', () => {
+    const restaurantList = <RestaurantList restaurants={[]} />
+    const tree = TestUtils.renderIntoDocument(<DataContainer children={restaurantList} />)
+    const component = TestUtils.findRenderedComponentWithType(tree, DataContainer)
+
+    spyOn(component, 'fetchRestaurantList').and.callThrough()
+    expect(component.fetchRestaurantList).not.toHaveBeenCalled()
+    component.componentDidMount()
+    expect(component.fetchRestaurantList).toHaveBeenCalled()
   })
 
   afterEach(() => {
